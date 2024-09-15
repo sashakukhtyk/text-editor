ec0cbba4256de9467cad09f2e54cbe7e9a9a0af9
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-09-15 12:03:32 +0300

    add some ui and open the window

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('editor.ui', self)
+        self.show()
+
+
+def main():
+    app = QApplication([])
+    window = MainWindow()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
